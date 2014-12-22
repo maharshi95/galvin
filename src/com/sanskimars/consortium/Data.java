da53079decc0bfda639b1db1d90c487a28b46eff
Maharshi Gor <maharshigor@yahoo.co.in>
2014-12-23 01:46:44 +0530

    Updates in Levels class and changing privte methods to public

--- src/com/sanskimars/consortium/Data.java
+++ src/com/sanskimars/consortium/Data.java
@@ -3,5 +3,8 @@ package com.sanskimars.consortium;
 public class Data {
 	public static final int LEVELS_PER_BUFFER = 5;
 	public static final int MAX_BUFFER = 5;
+	public static final int TIMEOUT_INCREMENT = 1000;
+	public static final int TIMEOUT_MIN = 3000;
+	public static final int TIMEOUT_MAX = 1000;
 	
 }
--- src/com/sanskimars/consortium/Level.java
+++ src/com/sanskimars/consortium/Level.java
@@ -13,34 +13,62 @@ public class Level {
 	private int score;
 	
 	public Level(int level) {
-		
+		timeout = getTimeout(level);
+		nsteps = getNSteps(level);
+		bufferSize = getBufferSize(level);
+		score = 0;
 	}
 	
-	private int timeout() {
+	public int timeout() {
 		return timeout;
 	}
 	
-	private int bufferSize() {
+	public int bufferSize() {
 		return bufferSize;
 	}
 	
-	private int nsteps() {
+	public int nsteps() {
 		return nsteps;
 	}
 	
-	private void addToScore(int stepScore) {
+	public void addToScore(int stepScore) {
 		score += stepScore;
 	}
 	
+	/**
+	 * 
+	 * @param level
+	 * @return timeout milliseconds for each step in the given level
+	 */
 	private static int getTimeout(int level) {
-		return 0;
+		int min = getTimeoutMinLimit(col(level));
+		int max = getTimeoutMaxLimit(col(level));
+		int r  = row(level);
+		int L = LEVELS_PER_BUFFER;
+		return (max*(L-r) + min*(r-1))/(L-1);
 	}
 	
+	private static int getTimeoutMinLimit(int col) {
+		return TIMEOUT_MIN + (col - 1)*TIMEOUT_INCREMENT;
+	}
+	
+	private static int getTimeoutMaxLimit(int col) {
+		return TIMEOUT_MAX  + (col-1)*TIMEOUT_INCREMENT;
+	}
+	
+	private static int col(int level) {
+		return (level-1)/LEVELS_PER_BUFFER + 1;
+	}
+	
+	private static int row(int level) {
+		return (level-1)%LEVELS_PER_BUFFER + 1;
+	}
 	private static int getBufferSize(int level) {
-		return(level - 1)/LEVELS_PER_BUFFER + 1;
+		return col(level);
 	}
 	
 	private static int getNSteps(int level) {
-		return 0;
+		return 10;
 	}
+	
 }
