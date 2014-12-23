dd64befac0d1c9e27dbd5e323e2e09e72ffd0253
Maharshi Gor <maharshigor@yahoo.co.in>
2014-12-23 20:02:30 +0530

    Implemenetd a timer model in main_activity Working successfully This model to be implemeted in each step of every level

--- src/com/sanskimars/consortium/MainActivity.java
+++ src/com/sanskimars/consortium/MainActivity.java
@@ -2,35 +2,75 @@ package com.sanskimars.consortium;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private CountDownTimer timer;
+	private Button playB;
+	private Button intB;
+	private EditText text;
+	private long time = 1000;
+	private long interval = 10;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		playB = (Button) findViewById(R.id.button_play);
+		intB = (Button) findViewById(R.id.button_interupt);
+		text = (EditText) findViewById(R.id.editText);
+		
+		intB.setEnabled(false);
+		text.setEnabled(false);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	public void playAction(View view) {
+		
+		playB.setEnabled(false);
+		intB.setEnabled(true);
+		timer = new CountDownTimer(time, interval) {
+			public void onTick(long timeLeft) {
+				text.setText("Time left: " + timeLeft);
+			}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+			public void onFinish() {
+				text.setText("Timer finished!!");
+				intB.setEnabled(false);
+				playB.setEnabled(true);
+			}
+		};
+		timer.start();
+	}
+
+	public void interuptAction(View view) {
+		intB.setEnabled(false);
+		text.setText("Timer interupted by user");
+		timer.cancel();
+		playB.setEnabled(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
